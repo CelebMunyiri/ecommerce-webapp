c8821477f6f09448f0af0563e38bb560f295ba35
David Munyiri <davidmumyiri2019@gmail.com>
2023-05-17 19:54:07 +0300

    added more styles and function on card

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import Card from './components/Card';
 
 function App() {
   return (
-    <div className="App">
-      <h1 style={{color:'blue'}}>Welcome to Abdullahi Dawud Store</h1>
+    <div>
+      <h1 style={{color:'blue',}}>Welcome to Abdullahi Dawud Store</h1>
+      <div className="App">
+      
+      <Card/>
       <Card/>
       <Card/>
     </div>
+      </div>
+    
+
+    
   );
 }
 
