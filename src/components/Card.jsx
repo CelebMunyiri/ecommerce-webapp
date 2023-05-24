068471fd3e27055c0948133ccc8c9ec90b3c2f95
David Munyiri <davidmumyiri2019@gmail.com>
2023-05-24 22:28:32 +0300

    handled show description button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -21,43 +21,43 @@ function Card(){
         {
         name: 'Kettle',
         cost: '$3.8',
-        description: 'Although Colvin is predominantly known for abstract themes that allude to pre-Hispanic symbols, this gigantic sculpture, an homage to neurosurgery, is one of her most recognizable public art pieces.',
+        description: 'Although to neurosurgery, is one of her most recognizable public art pieces.',
         image: 'https://i.imgur.com/Mx7dA2Y.jpg',
         alt: 'A bronze statue of two crossed hands delicately holding a human brain in their fingertips.'
       }, {
         name: 'Television',
         cost: '$15.4',
-        description: 'This enormous (75 ft. or 23m) silver flower is located in Buenos Aires. It is designed to move, closing its petals in the evening or when strong winds blow and opening them in the morning.',
+        description: 'This enormous when strong winds blow and opening them in the morning.',
         image: 'https://i.imgur.com/ZF6s192m.jpg',
         alt: 'A gigantic metallic flower sculpture with reflective mirror-like petals and strong stamens.'
       }, {
         name: 'Sneaker',
         cost: '$22.6',
-        description: 'Wilson was known for his preoccupation with equality, social justice, as well as the essential and spiritual qualities of humankind. This massive (7ft. or 2,13m) bronze represents what he described as "a symbolic Black presence infused with a sense of universal humanity."',
+        description: 'Wilson was known for his with a sense of universal humanity."',
         image: 'https://i.imgur.com/aTtVpES.jpg',
         alt: 'The sculpture depicting a human head seems ever-present and solemn. It radiates calm and serenity.'
       }, {
         name: 'Smart Watch',
         cost: '$13.2',
-        description: 'Located on the Easter Island, there are 1,000 moai, or extant monumental statues, created by the early Rapa Nui people, which some believe represented deified ancestors.',
+        description: 'Located on the Easter Island, there some believe represented deified ancestors.',
         image: 'https://i.imgur.com/RCwLEoQm.jpg',
         alt: 'Three monumental stone busts with the heads that are disproportionately large with somber faces.'
       }, {
         name: 'Man city Jersey',
         cost: '$90.2',
-        description: 'The Nanas are triumphant creatures, symbols of femininity and maternity. Initially, Saint Phalle used fabric and found objects for the Nanas, and later on introduced polyester to achieve a more vibrant effect.',
+        description: 'The Nanas are triumphant creatures, polyester to achieve a more vibrant effect.',
         image: 'https://i.imgur.com/Sd1AgUOm.jpg',
         alt: 'A large mosaic sculpture of a whimsical dancing female figure in a colorful costume emanating joy.'
       }, {
         name: 'Iphone Pro 12',
         cost: '$17.5',
-        description: 'This abstract bronze sculpture is a part of The Family of Man series located at Yorkshire Sculpture Park. Hepworth chose not to create literal representations of the world but developed abstract forms inspired by people and landscapes.',
+        description: 'This abstract bronze developed abstract forms inspired by people and landscapes.',
         image: 'https://i.imgur.com/2heNQDcm.jpg',
         alt: 'A tall sculpture made of three elements stacked on each other reminding of a human figure.'
       }, {
         name:'Sub Woofer',
         cost:'$32.9',
-        description: "Descended from four generations of woodcarvers, Fakeye's work blended traditional and contemporary Yoruba themes.",
+        description: "Descended from four generations temporary Yoruba themes.",
         image:'https://i.imgur.com/wIdGuZwm.png',
         alt:'An intricate wood sculpture of a warrior with a focused face on a horse adorned with patterns.'
       }
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Card.css'
 
 function Item({product}){
+  
+const [showDescription,setShowdescription]=useState(false)
+  
+const handleShowDescription=()=>{
+  setShowdescription(true)
+};
+const handleHideDescription=()=>{
+  setShowdescription(false)
+}
   return (
     
     <div className='Card'>
@@ -9,11 +18,21 @@ function Item({product}){
 
 <img src={product.image} alt={product.alt} className='img'/>
 <p style={{color:'blue'}}>{product.cost}</p>
-<p>{product.description}</p>
+
+<div>
+      {!showDescription ? (
+        <button onClick={handleShowDescription}>Show Description</button>
+      ) : (
+        <div>
+          <p>{product.description}</p>
+          <button onClick={handleHideDescription}>Hide Description</button>
+        </div>
+      )}
+    </div>
 
     </div>
-    
   )
+  
 }
 
 export default Item
