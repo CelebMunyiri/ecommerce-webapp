f652f078bf0de7f5c17c439085d288162c219c3b
David Munyiri <davidmumyiri2019@gmail.com>
2023-07-12 14:00:03 +0300

    encountered a bug in GroceriesChild component

--- src/components/Groceries/GroceriesChild.jsx
+++ src/components/Groceries/GroceriesChild.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { useState } from 'react';
 
-const GroceriesChild=()=> {
+const GroceriesChild=({product})=> {
+    const [cart,setCart]=useState() 
+const [showDescription,setShowdescription]=useState(false)
+  
+const handleShowDescription=()=>{
+  setShowdescription(true)
+};
+const handleHideDescription=()=>{
+  setShowdescription(false)
+}
+const addToCart = productId => {
+
+};
   return (
-    <div>
+    <div className='productCard'>
+        <img src={product.img} alt={product.name} className='imgCard' />
+        <h4>{product.name}</h4>
+        <h5>{product.price}</h5>
+        
+
+        <div>
+      {!showDescription ? (
+        <button onClick={handleShowDescription}>Show Description</button>
+      ) : (
+        <div>
+          <p>{product.description}</p>
+          <button onClick={handleHideDescription}>Hide Description</button>
+          
+        </div>
+
+      )}
+    </div>
+    
       
     </div>
   )
--- src/components/Groceries/GroceriesParent.jsx
+++ src/components/Groceries/GroceriesParent.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import GroceriesChild from './GroceriesChild'
 
 const GroceriesParent=()=> {
     const groceries=[
@@ -13,11 +14,14 @@ const GroceriesParent=()=> {
             img:'https://ke.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/30/285134/1.jpg?3579',
             description:'A 1 litre fanta passion soda by Cocacola soft drink manufacturers',
             price:'Ksh 184'
-        }
+        },
     ]
+    let ListOfGroceries=product.map(product=>
+<GroceriesChild key={product.name} product={product}/>
+    )
   return (
     <div>
-      
+      {ListOfGroceries}
     </div>
   )
 }
