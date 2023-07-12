f39b94a67788ca37f2e2f3003d596d248ed28099
David Munyiri <davidmumyiri2019@gmail.com>
2023-07-12 08:35:20 +0300

    solved the re-render error in all pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 //import { ListOfProducts } from './components/data';
 import Card from './components/Card';
 import {Route,Routes} from 'react-router-dom'
-// import Navbar from './components/Navbar';
+import Navbar from './components/Navbar';
 import Electronic from './components/Electronics/Electronic';
 import Groceries from './components/Groceries';
 import Furniture from './components/Furniture';
@@ -19,16 +19,13 @@ function App() {
    
     <div>
        <Routes>
-        <Route path='/home' element={<Home/>}/>
+        <Route path='/' element={<Home/>}/>
         <Route path='/electronic' element={<Electronic/>} />
         <Route path='/groceries' element={<Groceries/>}/>
         <Route path='/furniture' element={<Furniture/>}/>
         <Route path='/clothing' element={<Clothing/>}/>
       </Routes>
-      
-      {/* <Home/> */}
-      {/* <Navbar/>  */}
-      <Electronic/>
+
       </div>
   );
 }
